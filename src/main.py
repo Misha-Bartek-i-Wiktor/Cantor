3c1f91797127e309603eff06e8ab0a26d97a9d05
misha <mishasolop@gmail.com>
2020-07-07 19:33:34 +0200

    podłączono API z kursami walut. udało się przekonwertować json słownik w zwykłą linię. będzie trwała praca nad zdobyciem konkretnego klucza i znaczenia.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print('hello world!')
+import requests
+import json
+
+query = requests.get('https://api.exchangeratesapi.io/latest?base=USD')
+
+q = query.text
+
+
+media = json.loads(q)
+print(media)
