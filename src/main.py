6403995b7eb760893e687747119ff1eac95a7090
misha <mishasolop@gmail.com>
2020-07-08 20:11:03 +0200

    success 2!

--- src/main.py
+++ src/main.py
@@ -8,14 +8,14 @@ units_of_currency = float(input('How much? -> '))
 
 
 def func(base, final, units):
-    query = requests.get('https://api.exchangeratesapi.io/latest?base=' + base)
+    query = requests.get('https://api.exchangeratesapi.io/latest?base=' + base.upper())
 
     q = query.text
 
     media = json.loads(q)
 
     media2 = (media.get('rates'))
-    print(float(media2.get(final) * units))
+    print(float(media2.get(final.upper()) * units))
 
 
 func(base_currency, final_currency, units_of_currency)
